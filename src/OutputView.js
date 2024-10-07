eeed3412e1fc16a07df37c297ccb58241efe4451
wo-o29 <woogur29@gmail.com>
2024-10-07 21:53:14 +0900

    refactor: 인풋, 아웃풋 객체 동결

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,7 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
+export default OutputView = Object.freeze({
+  printMenu() {
+    Console.print("<주문 메뉴>");
     // ...
-}
+  },
+  // ...
+});
