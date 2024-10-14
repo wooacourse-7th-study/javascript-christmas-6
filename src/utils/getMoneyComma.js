b11e039991f2a6f1ba367c44ef654983d238b694
DevTeddy123 <ab315601@naver.com>
2024-10-14 22:19:47 +0900

    [refactor] : 가격 콤마 toLocaleString으로 변경

--- src/utils/getMoneyComma.js
+++ src/utils/getMoneyComma.js
@@ -1,6 +1,4 @@
 const getMoneyComma = beforeDiscountPrice => {
-	return (
-		beforeDiscountPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '원'
-	)
+	return beforeDiscountPrice.toLocaleString() + '원'
 }
 export default getMoneyComma
